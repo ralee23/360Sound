11b7b4f2a5634d15bffaf4dbb62fb1b746d54612
ROSS LEE <Rosslee@wicklowvec.ie>
2022-06-16 14:55:51 +0100

    360Sound

---
+++ data.js
@@ -0,0 +1,93 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sam_100_0061",
+      "name": "SAM_100_0061",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 1.711220065238316,
+        "pitch": 0.12768358554437853,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.32154024187843966,
+          "pitch": -0.21869265804285654,
+          "rotation": 0,
+          "target": "1-sam_sample_001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5599697494986877,
+          "pitch": -0.09878349373797768,
+          "title": "Books",
+          "text": "Our library stocks course relevant books to aid learning. Students can take books using their student card"
+        },
+        {
+          "yaw": -1.0410243263442887,
+          "pitch": -0.10477635722547518,
+          "title": "Computers",
+          "text": "Computers with relevant software are accessible to all with a student card and a college login account"
+        }
+      ]
+    },
+    {
+      "id": "1-sam_sample_001",
+      "name": "SAM_Sample_001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
